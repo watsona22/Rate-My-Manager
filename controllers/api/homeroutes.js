d8ba3552e4fa4c8a2b50d6c60573f26f0aa53184
Lady Godiva <watson.amber.denise@gmail.com>
2024-01-04 19:24:26 -0700

    updated login route and handlers

--- controllers/api/homeroutes.js
+++ controllers/api/homeroutes.js
@@ -23,4 +23,14 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect to the homepage
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    // Otherwise, render the 'login' template
+    res.render('login');
+});
+
 module.exports = router;
---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const { Rating } = require('../../models');
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
