b9642b7018ec4a966dc96eb8f58288dabbe7b1aa
Donnie1129 <drn112995@gmail.com>
2024-01-10 22:17:08 -0600

    fixed reviews page

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const {Rating} = require('../models');
 
 router.get('/login', (req, res) => {
     console.log("Loading login page!")
@@ -11,12 +12,6 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-router.get('/new', (req, res) => {
-
-    res.render('new');
-});
-module.exports = router;
-
 router.get('/signup', (req, res) => {
     // If the user is already logged in, redirect to the homepage
     if (req.session.loggedIn) {
@@ -27,11 +22,6 @@ router.get('/signup', (req, res) => {
     res.render('createAccount');
 });
 
-router.get('/new', (req, res) => {
-
-    res.render('new');
-});
-module.exports = router;
 
 router.get('/search', (req, res) => {
     // If the user is already logged in, redirect to the homepage
@@ -41,6 +31,19 @@ router.get('/search', (req, res) => {
     }
     // Otherwise, render the 'login' template
     res.render('search');
+});
+
+
+router.get('/reviews', async (req, res) => {
+    // If the user is already logged in, redirect to the homepage
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    // Otherwise, render the 'login' template
+    const ratingData = await Rating.findAll();
+  const ratings = ratingData.map((rating) => rating.get({ plain: true }));
+    res.render('reviews', {layout: 'main', ratings});
 });
 
 router.get('/new', (req, res) => {
