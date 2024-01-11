41d9233432d30c533d0d6892404cd3d1633470db
RoccaforteR <reeseroccaforter0@gmail.com>
2024-01-11 13:29:35 -0600

    loginDebugged

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,12 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
 
+      // Collect values from the login form
+     const email = document.querySelector('#email-login').value.trim();
+     const password = document.querySelector('#password-login').value.trim();
+
     if (email && password) {
-        const response = await fetch('/ratings/login', {
+        const response = await fetch('api/user/login', {
             method: 'POST',
             body: JSON.stringify({ email, password }),
             headers: { 'Content-Type': 'application/json' },
@@ -15,3 +19,7 @@ const loginFormHandler = async (event) => {
         }
     }
 };
+
+document
+  .querySelector('.loginForm')
+  .addEventListener('submit', loginFormHandler);
