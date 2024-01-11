4706127744f1b9952b5e1051d9465b3e7c61f068
MarkRBishop <mrb.8@hotmail.com>
2024-01-10 19:46:49 -0600

    changed the seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,25 +1,21 @@
 const sequelize = require('../config/connection');
-const { User, Review } = require('../models');
+const { User, Rating, Manager } = require('../models');
 
-const userData = require('./userData.json');
-const reviewData = require('./reviewData.json');
+const userData = require('./userData.js');
+const reviewData = require('./reviewData.js');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
-
   for (const review of reviewData) {
-    await review.create({
+    await Rating.create({
       ...review,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+      userId: users[Math.floor(Math.random() * users.length)].id,
     });
   }
-
   process.exit(0);
 };
-
 seedDatabase();
