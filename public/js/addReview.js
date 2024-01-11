7b3a3393af8ec12c2c816baaace9f680a9d5914b
MarkRBishop <mrb.8@hotmail.com>
2024-01-11 15:36:27 -0600

    editing the addReview.js

--- public/js/addReview.js
+++ public/js/addReview.js
@@ -1,7 +1,16 @@
 const addReview = async() => {
-    const response = await fetch('/ratings',{
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json'}
+  event.preventDefault()
+
+    const rating = document.getElementById('rating').value;
+    const managerName = document.getElementById('managerName').value.trim();
+    const userName = document.getElementById('userName').value.trim();
+    const comments = document.getElementById('comments').value.trim();
+
+    const response = await fetch('/api/ratings',{
+      method: 'POST',
+      body: JSON.stringify({ rating: parseInt(rating), managerName, userName, comments }),
+      headers: { 'Content-Type': 'application/json'},
+
     }); 
 
     if (response.ok) {
